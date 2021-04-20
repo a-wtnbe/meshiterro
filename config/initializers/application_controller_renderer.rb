d83a2feaceb0b026b8c9310fb944a864d1d0b520
a-wtnbe <ayawatanabe714@gmail.com>
2021-04-20 05:20:50 +0000

    start6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '61da9a0e6a70b9df5c1c68feafbb052470b6166c811ed31cdaaceb26c0998571bfd787b8af68f989b36ff32d486f7384fd9b01d3e00d07d3d9f66e9be5332b6e'
